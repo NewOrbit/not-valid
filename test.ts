2f31d12c7b608b0b2c50231044e115b0d6460687
James Monger <jameskmonger@hotmail.co.uk>
2017-05-08 10:59:17 +0100

    Create tests

---
+++ test.ts
@@ -0,0 +1,74 @@
+import { TestFixture, Test, TestCase, Expect } from "alsatian";
+import { validate } from "./index";
+
+@TestFixture()
+export class ValidationTests {
+
+    @Test()
+    public shouldReturnNullIfNoValidators() {
+        const result = validate([], 0);
+
+        Expect(result).toBe(null);
+    }
+
+    @TestCase("an error occured")
+    @TestCase("foo")
+    public shouldReturnErrorMessageForOneError(message: string) {
+        const result = validate([
+            () => message            
+        ], 0);
+
+        Expect(result).toEqual([ message ]);
+    }
+
+    @TestCase("foo", "baz baz baz")
+    @TestCase("404 document not found", "ice cream and brie")
+    public shouldReturnErrorMessageForTwoErrors(firstMessage: string, secondMessage: string) {
+        const result = validate([
+            () => firstMessage, () => secondMessage            
+        ], 0);
+
+        Expect(result).toEqual([ firstMessage, secondMessage ]);
+    }
+
+    @TestCase("baz baz baz")
+    @TestCase("ice cream and brie")
+    public shouldNotReturnErrorMessageForValidatorWhichReturnsNull(secondMessage: string) {
+        const result = validate([
+            () => null, () => secondMessage            
+        ], 0);
+
+        Expect(result).toEqual([ secondMessage ]);
+    }
+
+    @TestCase(1)
+    @TestCase(2)
+    public shouldCallFirstValidatorWithValue(value: number) {
+        let calledCorrectly = false;
+        const validator = (receivedValue: number) => {
+            if (receivedValue === value) {
+                calledCorrectly = true;
+            }
+        };
+
+        validate([
+            validator, () => null            
+        ], value);
+    }
+
+    @TestCase(1)
+    @TestCase(2)
+    public shouldCallSecondValidatorWithValue(value: number) {
+        let calledCorrectly = false;
+        const validator = (receivedValue: number) => {
+            if (receivedValue === value) {
+                calledCorrectly = true;
+            }
+        };
+
+        validate([
+            () => null, validator            
+        ], value);
+    }
+
+}
