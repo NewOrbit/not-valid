6983734e94e1615d165ef16e450e85fa5ddd8a6f
James Monger <jameskmonger@hotmail.co.uk>
2017-05-08 11:49:49 +0100

    Strictly type return type

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 type ValidationResult = string | null;
 type ValidationFunction<T> = (value: T) => ValidationResult;
 
-const validate = <T>(validators: Array<ValidationFunction<T>>, value: T) => {
+const validate: <T>(validators: Array<ValidationFunction<T>>, value: T) => Array<ValidationResult>
+    = <T>(validators: Array<ValidationFunction<T>>, value: T) => {
     const errors: Array<ValidationResult> = [];
 
     validators.forEach(validator => {
