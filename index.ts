18fbdfbea4f6a9fedead33bb53b901ee0cffba77
James Monger <jameskmonger@hotmail.co.uk>
2017-05-16 17:00:01 +0100

    Implement sequential option

--- index.ts
+++ index.ts
@@ -2,19 +2,45 @@ type ValidationResult = string | null;
 type ValidationPredicate<T> = (value: T) => boolean;
 type ValidationFunction<T> = (value: T) => ValidationResult;
 
-const validate: <T>(validators: Array<ValidationFunction<T>>, value: T) => Array<string>
-    = <T>(validators: Array<ValidationFunction<T>>, value: T) => {
+interface ValidationOptions {
+    sequential?: boolean;
+}
+
+const DEFAULT_OPTIONS: ValidationOptions = {
+    sequential: false
+};
+
+const getOptions = (options?: ValidationOptions) => {
+    if (options === undefined) {
+        return {
+            sequential: DEFAULT_OPTIONS.sequential
+        };
+    }
+
+    return {
+        sequential: options.sequential || DEFAULT_OPTIONS.sequential
+    };
+};
+
+const validate: <T>(validators: Array<ValidationFunction<T>>, value: T, options?: ValidationOptions) => Array<string>
+    = <T>(validators: Array<ValidationFunction<T>>, value: T, options?: ValidationOptions) => {
+
+    const opts = getOptions(options);
 
     const errors: Array<string> = [];
 
-    validators.forEach(validator => {
+    for (const validator of validators) {
         const result = validator(value);
 
         // if the validator doesn't return null, then there was an error
         if (result !== null) {
             errors.push(result);
+
+            if (opts.sequential) {
+                break;
+            }
         }
-    });
+    }
 
     return errors;
 };
@@ -35,6 +61,7 @@ export {
     ValidationResult,
     ValidationPredicate,
     ValidationFunction,
+    ValidationOptions,
     validate,
     createValidator
 };
