deadfbdb3f5045d1dbf0940df1899db9f8539a2b
James Monger <jameskmonger@hotmail.co.uk>
2017-10-10 12:32:05 +0100

    Implement createValidatorFactory

--- src/create-validator-factory.ts
+++ src/create-validator-factory.ts
@@ -2,16 +2,14 @@ import { ValidationResult, Result } from "./results/validation-result";
 import { ValidationPredicate, AsyncValidationFunction, SyncValidationFunction } from "./types";
 
 const createValidatorFactory = <T>(predicate: ValidationPredicate<T>) => {
-    /*const validationFunction: SyncValidationFunction<T> = (value: T, message: string) => {
-        const result = predicate(value);
-
-        return result ? Result.Pass : Result.Fail(message);
-    };
-
-    return validationFunction;*/
-
     return (message: string) => {
-        return (value: T) => undefined;
+        const validationFunction = (value: T) => {
+            const result = predicate(value);
+    
+            return result ? Result.Pass : Result.Fail(message);
+        };
+        
+        return (value: T) => validationFunction(value);
     };
 };
 
--- test/create-validator-factory.test.ts
+++ test/create-validator-factory.test.ts
@@ -1,8 +1,7 @@
-import { TestFixture, Test, TestCase, Expect, SpyOn, FocusTests } from "alsatian";
+import { TestFixture, Test, TestCase, Expect, SpyOn } from "alsatian";
 import { Result } from "../src/index";
 import { createValidatorFactory } from "../src/create-validator-factory";
 
-@FocusTests
 @TestFixture()
 export class CreateValidatorFactoryTests {
 
@@ -15,39 +14,24 @@ export class CreateValidatorFactoryTests {
 
         SpyOn(spyWrapper, "predicate");
 
-        const factory = createValidatorFactory(predicate);
+        const factory = createValidatorFactory(spyWrapper.predicate);
+        const validator = factory("some message");
+        
         const input = 0;
-
-        factory("some message")(input);
+        validator(input);
 
         Expect(spyWrapper.predicate).toHaveBeenCalledWith(input);
     }
 
-    /*
-    @TestCase((s: number) => s !== 1, 1, false)
-    @TestCase((s: number) => s !== 1, 2, true)
-    @TestCase((s: string) => s.length === 3, "foo", true)
-    @TestCase((s: string) => s.indexOf("james") !== -1, "I am bob", false)
-    public shouldUseCorrectPredicate(predicate: (val: any) => boolean, input: any, shouldPass: boolean) {
-        const message = "failed";
-        const validator = createValidator<any>(predicate, message);
+    @TestCase("message one")
+    @TestCase("another message")
+    public shouldFailWithCorrectMessage(message: string) {
+        const factory = createValidatorFactory(() => false); // create a validator that always fails
+        const validator = factory(message);
 
-        const expectedResult = shouldPass ? Result.Pass : Result.Fail(message);
-
-        const result = validator(input);
-
-        Expect(result).toEqual(expectedResult);
-    }
-
-    @TestCase("it aint no good yo")
-    @TestCase("FAILURE FAILURE 0x100")
-    public shouldUseCorrectMessage(message: string) {
-        const validator = createValidator<number>((s: number) => s !== 5, message);
-
-        // make validation fail
-        const result = validator(5);
+        const result = validator(0); // call the validator with anything - it always fails
 
         Expect(result).toEqual(Result.Fail(message));
     }
-    */
+
 }
