2f8836a8b8d8654e27332b41f014066b8ade7f98
James Monger <jameskmonger@hotmail.co.uk>
2017-10-10 12:45:36 +0100

    Remove createAsyncValidatorFactory

--- src/create-validator-factory.ts
+++ src/create-validator-factory.ts
@@ -1,18 +1,27 @@
 import { ValidationResult, Result } from "./results/validation-result";
 import { ValidationPredicate, AsyncValidationFunction, SyncValidationFunction } from "./types";
 
-const createValidatorFactory = <T>(predicate: ValidationPredicate<T>) => {
-    return (message: string) => {
-        const validationFunction = (value: T) => {
+const createValidatorFactory = <T>(predicate: (value: T) => boolean) => {
+    return (message: string) => {        
+        return (value: T) => {
             const result = predicate(value);
     
             return result ? Result.Pass : Result.Fail(message);
         };
-        
-        return (value: T) => validationFunction(value);
+    };
+};
+
+const createAsyncValidatorFactory = <T>(predicate: (value: T) => Promise<boolean>) => {
+    return (message: string) => {
+        return async (value: T) => {
+            const result = await predicate(value);
+    
+            return result ? Result.Pass : Result.Fail(message);
+        };
     };
 };
 
 export {
-    createValidatorFactory
+    createValidatorFactory,
+    createAsyncValidatorFactory
 };
--- test/create-validator-factory.test.ts
+++ test/create-validator-factory.test.ts
@@ -1,6 +1,6 @@
-import { TestFixture, Test, TestCase, Expect, SpyOn } from "alsatian";
+import { TestFixture, Test, TestCase, Expect, SpyOn, AsyncTest } from "alsatian";
 import { Result } from "../src/index";
-import { createValidatorFactory } from "../src/create-validator-factory";
+import { createValidatorFactory, createAsyncValidatorFactory } from "../src/create-validator-factory";
 
 @TestFixture()
 export class CreateValidatorFactoryTests {
@@ -30,6 +30,42 @@ export class CreateValidatorFactoryTests {
         const validator = factory(message);
 
         const result = validator(0); // call the validator with anything - it always fails
+
+        Expect(result).toEqual(Result.Fail(message));
+    }
+
+}
+
+@TestFixture()
+export class CreateAsyncValidatorFactoryTests {
+
+    @AsyncTest()
+    @TestCase(async (x: number) => x === 0)
+    @TestCase(async (y: number) => y === y * 2)
+    public async shouldUseCorrectPredicate(predicate: () => Promise<boolean>) {
+        const spyWrapper = {
+            predicate: predicate
+        };
+
+        SpyOn(spyWrapper, "predicate");
+
+        const factory = createAsyncValidatorFactory(spyWrapper.predicate);
+        const validator = factory("some message");
+        
+        const input = 0;
+        await validator(input);
+
+        Expect(spyWrapper.predicate).toHaveBeenCalledWith(input);
+    }
+
+    @AsyncTest()
+    @TestCase("message one")
+    @TestCase("another message")
+    public async shouldFailWithCorrectMessage(message: string) {
+        const factory = createAsyncValidatorFactory(async () => false); // create a validator that always fails
+        const validator = factory(message);
+
+        const result = await validator(0); // call the validator with anything - it always fails
 
         Expect(result).toEqual(Result.Fail(message));
     }
