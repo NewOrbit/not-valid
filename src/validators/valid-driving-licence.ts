7e2e32ee352e11541160a8b42ed0b41f27bbc649
David Gruar <davidgruar@gmail.com>
2018-03-07 12:35:14 +0000

    Add options to driving licence validation and tighten existing regex

--- src/validators/valid-driving-licence.ts
+++ src/validators/valid-driving-licence.ts
@@ -1,7 +1,76 @@
-import { ValidationResult } from "../results/validation-result";
-import { createValidatorFactory } from "../create-validator";
+import { createValidator } from "../create-validator";
 import { DRIVING_LICENCE } from "../messages";
+import { SyncValidationFunction } from "..";
 
-const DRIVING_LICENCE_REGEX = /([a-zA-Z0-9]{5})([0-9]{6})([a-zA-Z0-9]{5})([0-9]{2})/m;
+const regexes = {
+    withIssueNumber: /^([a-zA-Z0-9]{5})([0-9]{6})([a-zA-Z0-9]{5})([0-9]{2})$/,
+    withoutIssueNumber: /^([a-zA-Z0-9]{5})([0-9]{6})([a-zA-Z0-9]{5})$/,
+    northernIreland: /^\d{8}$/
+};
 
-export default createValidatorFactory<string>(x => DRIVING_LICENCE_REGEX.test(x), DRIVING_LICENCE);
+export type IssueNumberOptions = "required" | "optional" | "prohibited";
+
+export interface DrivingLicenceOptions {
+    message?: string;
+    allowNI?: boolean;
+    issueNumber?: IssueNumberOptions;
+}
+
+const defaultOptions: DrivingLicenceOptions = {
+    allowNI: false,
+    issueNumber: "optional"
+};
+
+function validateGB(value: string, options: DrivingLicenceOptions) {
+    const { withIssueNumber, withoutIssueNumber } = regexes;
+    switch (options.issueNumber) {
+        case "required":
+            return withIssueNumber.test(value);
+        case "prohibited":
+            return withoutIssueNumber.test(value);
+        default:
+            return withIssueNumber.test(value) || withoutIssueNumber.test(value);
+    }
+}
+
+function validateNI(value: string) {
+    return regexes.northernIreland.test(value);
+}
+
+function validate(value: string, options: DrivingLicenceOptions) {
+    const isValidGBNumber = validateGB(value, options);
+    if (!options.allowNI) {
+        return isValidGBNumber;
+    }
+    return isValidGBNumber || validateNI(value);
+}
+
+function createWithOptions(options = defaultOptions) {
+    return createValidator<string>(x => validate(x, options), options.message || DRIVING_LICENCE);
+}
+
+function createWithString(message?: string) {
+    return createWithOptions({ ...defaultOptions, message });
+}
+
+/**
+ * Creates a driving licence validator with the default options and the provided message, or the default message if none is provided.
+ * @param  {string} message The error message
+ */
+function create(message?: string): SyncValidationFunction<string>;
+
+/**
+ * Creates a driving licence validator with the provided options.
+ * @param {string} options.message A custom error message.
+ * @param {string} options.allowNI Whether to allow Northern Ireland number formats. Default: false.
+ * @param {IssueNumberOptions} options.message How to treat the issue number (the last two digits). Default: "optional".
+ */
+function create(options: DrivingLicenceOptions): SyncValidationFunction<string>;
+
+function create(messageOrOptions?: string | DrivingLicenceOptions): SyncValidationFunction<string> {
+    return typeof messageOrOptions === "string"
+        ? createWithString(messageOrOptions as string)
+        : createWithOptions(messageOrOptions as DrivingLicenceOptions);
+}
+
+export default create;
--- test/validators/valid-driving-licence.test.ts
+++ test/validators/valid-driving-licence.test.ts
@@ -1,12 +1,15 @@
 import { TestFixture, Test, TestCase } from "alsatian";
 import { Expect } from "../utils/alsatian";
-import validDrivingLicence from "../../src/validators/valid-driving-licence";
+import validDrivingLicence, { DrivingLicenceOptions } from "../../src/validators/valid-driving-licence";
+import { DRIVING_LICENCE } from "../../src/messages";
 
 @TestFixture()
 export class ValidDrivingLicenseTests {
 
     @TestCase("ABCDE948174ABCDE12")
     @TestCase("XYZAS281944IFJEE94")
+    @TestCase("ABCDE948174AB1DE")
+    @TestCase("XYZAS281944IFJEE")
     public shouldPassForValidDrivingLicence(value: string) {
         const validator = validDrivingLicence("failure message");
         const results = validator(value);
@@ -17,10 +20,51 @@ export class ValidDrivingLicenseTests {
     @TestCase(null)
     @TestCase("     ")
     @TestCase(false)
+    @TestCase("ABCDE948174ABCD")
+    @TestCase("12345AAAAA12ABC")
+    @TestCase("fooABCDE948174ABCDE12bar")
+    @TestCase("12345678")
     public shouldFailForInvalidDrivingLicence(value: any) {
         const failureMessage = "failure message";
         const validator = validDrivingLicence(failureMessage);
         const results = validator(value);
         Expect(results).toBeAFailWithMessage(failureMessage);
+    }
+
+    @TestCase("ABCDE123456EF7GH", { issueNumber: "optional" })
+    @TestCase("ABCDE123456EF7GH", { issueNumber: "prohibited" })
+    @TestCase("ABCDE123456EF7GH12", { issueNumber: "optional" })
+    @TestCase("ABCDE123456EF7GH12", { issueNumber: "required" })
+    @TestCase("12345678", { allowNI: true })
+    @TestCase("ABCDE123456EF7GH", { allowNI: true })
+    @TestCase("ABCDE123456EF7GH12", { allowNI: true })
+    public shouldPassForValidDrivingLicenceWithOptions(value: string, options: DrivingLicenceOptions) {
+        const validator = validDrivingLicence(options);
+        const results = validator(value);
+        Expect(results).toBeAPass();
+    }
+
+    @TestCase("ABCDE123456EF7GH", { issueNumber: "required" })
+    @TestCase("ABCDE123456EF7GH12", { issueNumber: "prohibited" })
+    @TestCase("12345678", { allowNI: false })
+    @TestCase("1234567", { allowNI: true })
+    @TestCase("123456789", { allowNI: true })
+    public shouldFailForInvalidDrivingLicenceWithOptions(value: any, options: DrivingLicenceOptions) {
+        const failureMessage = "failure message";
+        const validator = validDrivingLicence({ ...options, message: failureMessage });
+        const results = validator(value);
+        Expect(results).toBeAFailWithMessage(failureMessage);
+    }
+
+    public shouldUseDefaultErrorMessageIfNoneIsProvided() {
+        const validator = validDrivingLicence();
+        const results = validator("foo bar");
+        Expect(results).toBeAFailWithMessage(DRIVING_LICENCE);
+    }
+
+    public shouldUseDefaultErrorMessageIfNoneIsProvidedInOptions() {
+        const validator = validDrivingLicence({ issueNumber: "optional" });
+        const results = validator("foo bar");
+        Expect(results).toBeAFailWithMessage(DRIVING_LICENCE);
     }
 }
