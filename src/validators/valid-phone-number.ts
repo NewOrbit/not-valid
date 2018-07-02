b978c63b9f8d478f093bab241fa4d09e936fefe3
Tom Hyde <thyde@neworbit.co.uk>
2018-07-02 17:30:28 +0100

    Added a mandatory min length

--- src/validators/valid-phone-number.ts
+++ src/validators/valid-phone-number.ts
@@ -1,16 +1,17 @@
 import { ValidationResult } from "../results/validation-result";
-import { createValidatorFactory } from "../create-validator";
 import { INVALID_PHONE_NUMBER } from "../messages";
+import { createValidator } from "..";
 
-const PHONE_NUMBER_REGEX = /^(0{0,2}|\+|[1-9])[1-9]\d{7,}$/;
+const PHONE_NUMBER_REGEX = /^(0{0,2}|\+|[1-9])[1-9]\d*$/;
 
-export default createValidatorFactory<string>(
-    phoneNumber => {
+export default (minLength: number, message?: string) => {
+    return createValidator<string>((phoneNumber: string) => {
         phoneNumber = phoneNumber.replace(/ /g, "");
-        if (phoneNumber.length < 10) {
+        if (phoneNumber.length < minLength) {
             return false;
         }
 
         return !phoneNumber || PHONE_NUMBER_REGEX.test(phoneNumber);
     },
-    INVALID_PHONE_NUMBER);
+    message || INVALID_PHONE_NUMBER);
+}
