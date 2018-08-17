ac4b7245a2305dcb19eccdbd3b0cf7cc492b13c2
Justin Chew <jchew@neworbit.co.uk>
2018-08-17 11:36:26 +0100

    Fix naming and export validation

--- src/validators/valid-account-number.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationResult } from "../results/validation-result";
-import { createRegexValidator } from "../create-regex-validator";
-import { INVALID_ACCOUNT_NUMBER } from "../messages";
-
-const ACCOUNT_NUMBER_REGEX = /^\d{8}$/;
-
-export const validAccountNumber = createRegexValidator(ACCOUNT_NUMBER_REGEX, INVALID_ACCOUNT_NUMBER);
