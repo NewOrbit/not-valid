63639ea9a6138333e0ee30563d702277ea2b1a6b
Justin Chew <jchew@neworbit.co.uk>
2018-08-17 10:53:17 +0100

    Fix naming and messages

--- src/validators/valid-account-number.ts
+++ src/validators/valid-account-number.ts
@@ -1,7 +1,7 @@
 import { ValidationResult } from "../results/validation-result";
 import { createRegexValidator } from "../create-regex-validator";
-import { VALID_ACCOUNT_NUMBER } from "../messages";
+import { INVALID_ACCOUNT_NUMBER } from "../messages";
 
 const ACCOUNT_NUMBER_REGEX = /^\d{8}$/;
 
-export const validAccountNumber = createRegexValidator(ACCOUNT_NUMBER_REGEX, VALID_ACCOUNT_NUMBER);
+export const validAccountNumber = createRegexValidator(ACCOUNT_NUMBER_REGEX, INVALID_ACCOUNT_NUMBER);
--- src/validators/valid-vat-number.ts
+++ src/validators/valid-vat-number.ts
@@ -1,7 +1,7 @@
 import { ValidationResult } from "../results/validation-result";
 import { createRegexValidator } from "../create-regex-validator";
-import { VALID_VAT_NUMBER } from "../messages";
+import { INVALID_VAT_NUMBER } from "../messages";
 
 const VAT_NUMBER_REGEX = /^\d{9}$/;
 
-export const validVatNumber = createRegexValidator(VAT_NUMBER_REGEX, VALID_VAT_NUMBER);
+export const validVatNumber = createRegexValidator(VAT_NUMBER_REGEX, INVALID_VAT_NUMBER);
