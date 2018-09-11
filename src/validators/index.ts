7ab0706134a308a1475970d2ba2eff2d32ae2482
James Monger <jameskmonger@hotmail.co.uk>
2018-09-11 14:43:10 +0100

    Reset version to 1.0.0

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,28 +1,27 @@
 import { requiredString } from "./required-string";
 import { requiredNumber } from "./required-number";
-
-import { validEmail } from "./valid-email";
-import { validSortCode } from "./valid-sort-code";
-import { validOption } from "./valid-option";
-import { validUKDrivingLicence } from "./valid-driving-licence";
-import { validNINumber } from "./valid-ni-number";
 import { validLength } from "./valid-length";
-import { validPhoneNumber } from "./valid-phone-number";
+import { validEmail } from "./valid-email";
 import { validAlphanumeric } from "./valid-alphanumeric";
+import { validOption } from "./valid-option";
+import { validPhoneNumber } from "./valid-phone-number";
+import { validNINumber } from "./valid-ni-number";
+import { validUKDrivingLicence } from "./valid-driving-licence";
+import { validSortCode } from "./valid-sort-code";
 import { validBankAccountNumber } from "./valid-bank-account-number";
 import { validVATNumber } from "./valid-vat-number";
 
 export {
-    validUKDrivingLicence,
     requiredString,
-    validEmail,
     requiredNumber,
-    validSortCode,
-    validOption,
-    validNINumber,
     validLength,
-    validPhoneNumber,
+    validEmail,
     validAlphanumeric,
+    validOption,
+    validPhoneNumber,
+    validNINumber,
+    validUKDrivingLicence,
+    validSortCode,
     validBankAccountNumber,
     validVATNumber
 };
